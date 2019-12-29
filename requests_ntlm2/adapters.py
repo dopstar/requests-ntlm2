7b0965fc1d19ab67c9a1734155645795ca1ca853
Mka Madlavana <dopstar@users.noreply.github.com>
2019-12-29 16:50:11 +0200

    fix the host header mess once and for all (#3)

--- requests_ntlm2/adapters.py
+++ requests_ntlm2/adapters.py
@@ -1,3 +1,8 @@
+try:
+    import urlparse
+except ImportError:
+    import urllib.parse as urlparse
+
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.connection import HTTPConnection, HTTPSConnection
 from requests.packages.urllib3.poolmanager import pool_classes_by_scheme
@@ -6,7 +11,47 @@ from .connection import HTTPConnection as _HTTPConnection
 from .connection import HTTPSConnection as _HTTPSConnection
 
 
-class HttpNtlmAdapter(HTTPAdapter):
+class HttpProxyAdapter(HTTPAdapter):
+    def _add_host_header(self, request):
+        if request.headers.get("Host"):
+            if self._is_valid_host_header(request):
+                return
+            else:
+                self._remove_host_header(request)
+
+        parse_result = urlparse.urlparse(request.url)
+        if parse_result.scheme == "http":
+            if parse_result.port == 80:
+                request.headers["Host"] = parse_result.hostname
+            else:
+                request.headers["Host"] = parse_result.netloc
+
+    @staticmethod
+    def _is_valid_host_header(request):
+        host = request.headers.get("Host")
+        if not host:
+            return False
+        parse_result = urlparse.urlparse(request.url)
+        if parse_result.scheme == "http" and parse_result.port == 80:
+            return False
+        elif parse_result.scheme == "https":
+            if host == parse_result.netloc and parse_result.port is not None:
+                return True
+        return False
+
+    @staticmethod
+    def _remove_host_header(request):
+        try:
+            del request.headers["Host"]
+        except KeyError:
+            pass
+
+    def add_headers(self, request, **kwargs):
+        super(HttpProxyAdapter, self).add_headers(request, **kwargs)
+        self._add_host_header(request)
+
+
+class HttpNtlmAdapter(HttpProxyAdapter):
     def __init__(self, ntlm_username, ntlm_password, *args, **kwargs):
         """
         Thin wrapper around requests.adapters.HTTPAdapter
