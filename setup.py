c0288bd3cde87ac97777a61e43fc8e8665685b24
Mka Madlavana <mkhanyisi.madlavana@hpe.com>
2020-09-29 19:02:55 +0200

    stop testing on py3.5

--- setup.py
+++ setup.py
@@ -68,7 +68,6 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
